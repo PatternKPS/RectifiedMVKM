a86e35964baa478b51d2493f41fb286f67e86c33
Kristina P. Sinaga <150363044+PatternKPS@users.noreply.github.com>
2024-05-09 17:31:07 +0700

    Add files via upload

---
+++ run_me_first.m
@@ -0,0 +1,31 @@
+%%   This package is provided (written and created) by Kristina P. Sinaga 
+%%   Copyright (c) 2023 Kristina P. Sinaga 
+%%   All rights reserved
+%%   Contact :  krist.p.sinaga@gmail.com
+%%   ------------------------------------------------------------------- %%
+
+clear all;close all;clc
+% Add folders to path.
+
+addpath(pwd);
+
+
+cd Dataset/;
+addpath(genpath(pwd));
+cd ..;
+
+cd GKMVKM/;
+addpath(genpath(pwd));
+cd ..;
+
+cd MVKMED/;
+addpath(genpath(pwd));
+cd ..;
+
+cd measurement/;
+addpath(genpath(pwd));
+cd ..;
+
+cd tools/;
+addpath(genpath(pwd));
+cd ..;
